589fbf84c60915ad2d6adc8a838367d0e7560d29
c0ffeeca7 <g@briela.ch>
2023-02-07 09:44:21 +0100

    Add draft SkyConnect setup procedures:
    - Initial setup for new Zigbee network
    - Migration from Z2M to Home Assistan SkyConnect
    
    Align web-template with template of Home Assistant Yellow page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const yaml = require("js-yaml");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
   eleventyConfig.addPassthroughCopy({
     "public/images": "images",
     "public/css": "css",
