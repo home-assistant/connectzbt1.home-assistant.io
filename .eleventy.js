a9c8fbca5c7d250a9255065d62be83a0ebf40405
Paulus Schoutsen <balloob@gmail.com>
2023-02-08 14:39:49 -0500

    Allow updating firmware from the website

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   eleventyConfig.addPassthroughCopy({
     "public/images": "images",
     "public/css": "css",
+    "public/flasher.json": "flasher.json",
   });
 
   return {
